e842defd02dc4ebd20eb1e8d489d2b398e5b808c
Matheus Sena Rosa <matheussenarosa8@gmail.com>
2022-07-27 08:57:25 -0300

    💫 add transition between screens

--- src/routes/Public.routes.tsx
+++ src/routes/Public.routes.tsx
@@ -1,13 +1,39 @@
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { CardStyleInterpolators, createStackNavigator } from "@react-navigation/stack";
 import { LogIn, SignUp } from "@screens";
+import { Easing } from "react-native";
 import { propsNavigationStack } from "./models";
 
 const { Navigator, Screen } =
-  createNativeStackNavigator<propsNavigationStack>();
+createStackNavigator<propsNavigationStack>();
 
 export function PublicRoutes() {
   return (
-    <Navigator initialRouteName="LogIn" screenOptions={{ headerShown: false }}>
+    <Navigator
+      initialRouteName="LogIn"
+      screenOptions={{
+        headerShown: false,
+        transitionSpec: {
+          open: {
+            animation: "spring",
+            config: {
+              stiffness: 1000,
+              damping: 50,
+              overshootClamping: false,
+              restDisplacementThreshold: 0.01,
+              restSpeedThreshold: 0.01,
+            },
+          },
+          close: {
+            animation: "timing",
+            config: {
+              duration: 200,
+              easing: Easing.linear,
+            },
+          },
+        },
+        cardStyleInterpolator: CardStyleInterpolators.forHorizontalIOS,
+      }}
+    >
       <Screen name="LogIn" component={LogIn} />
       <Screen name="SignUp" component={SignUp} />
     </Navigator>
